dad80760a29d1dbc737266fc1316e4a3739504e9
Lauri Methuen <lauri.methuen@gmail.com>
2019-10-12 22:30:00 +0300

    Add memoize func extension

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Realtime.Messaging.Internal;
+
+public static class ExtensionMethods {
+
+	public static Func<T, TResult> Memoize<T, TResult>(this Func<T, TResult> f) {
+		var cache = new ConcurrentDictionary<T, TResult>();
+		return key => cache.GetOrAdd(key, f);
+	}
+
+}
