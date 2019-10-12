f3a8b845194531f4db9e8c88b9462dda677ec8d4
Lauri Methuen <lauri.methuen@gmail.com>
2019-10-12 22:31:10 +0300

    Fix DrawNormals and improve its' performance

--- Assets/Scripts/Debug/DrawNormals.cs
+++ Assets/Scripts/Debug/DrawNormals.cs
@@ -8,20 +8,37 @@ public class DrawNormals: MonoBehaviour {
 	Mesh mesh;
 	Vector3[] vertices;
 	Vector3[] normals;
+	float nextActionTime = 0.0f;
+	readonly float period = 5;
+
+	Matrix4x4 localToWorld;
 
 
 	void Start () {
+		localToWorld = transform.localToWorldMatrix;
 		mf = transform.GetComponent<MeshFilter>();
 		mesh = mf.mesh;
 		vertices = mesh.vertices;
 		normals = mesh.normals;
 	}
 
-	
-	void FixedUpdate () {
-		for (int iii = 0; iii < vertices.Length; ++iii) {
-			Debug.DrawLine(transform.position + transform.localRotation * vertices[iii] + transform.localRotation * normals[iii] * 9.5f, transform.position + transform.localRotation * vertices[iii] + transform.localRotation * (normals[iii] * 10.5f), Color.green);
-			Debug.DrawLine(transform.position + transform.localRotation * vertices[iii] + transform.localRotation * normals[iii] * 10.5f, transform.position + transform.localRotation * vertices[iii] + transform.localRotation * (normals[iii] * 11), Color.yellow);
+	void Update() {
+		if (Time.time > nextActionTime) {
+			nextActionTime += period;
+			for (int iii = 0; iii < vertices.Length; ++iii) {
+				Debug.DrawLine(
+					transform.position + localToWorld.MultiplyVector(vertices[iii]),
+					transform.position + localToWorld.MultiplyVector(vertices[iii]) * 1.025f,
+					Color.green,
+					period
+				);
+				Debug.DrawLine(
+					transform.position + localToWorld.MultiplyVector(vertices[iii]) * 1.025f,
+					transform.position + localToWorld.MultiplyVector(vertices[iii]) * 1.05f,
+					Color.yellow,
+					period
+				);
+			}
 		}
 	}
 
